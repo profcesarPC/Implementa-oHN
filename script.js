417dc2d4c58aca71c54d8febf8ec49b08807f344
Cesar Pereira <105113840+profcesarPC@users.noreply.github.com>
2025-07-03 15:57:22 -0300

    criação de pagina

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript: adiciona interatividade
+function mudarImagem() {
+    const imagem = document.getElementById('imagem');
+    imagem.src = 'nova-imagem.jpg'; // Substitua pelo nome real da nova imagem
+}
